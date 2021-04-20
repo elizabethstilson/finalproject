7eb4a855321c49b6cda832b296cc23b01a84f734
rebeccafagan52 <80499789+rebeccafagan52@users.noreply.github.com>
2021-04-19 21:32:15 -0400

    Update Lane.h

--- Lane.h
+++ Lane.h
@@ -1,8 +1,23 @@
 #include <string>
 #include <vector>
 
+#include "VehicleBase.h"
+#include "Section.h"
+
 
 class Lane{
   private:
-    static int
+    double numSections;
+    Direction laneDirection;
+
+  public:
+    vector<Section> lane;
+    Lane(int halfsize, Direction direction);
+    ~Lane();
+    void assignVehicle(VehicleBase vehicle);
+    void moveLane(LightColor light);
+    void turnRight(VehicleBase vehicle, Lane turnLane);
+
+
 }
+
