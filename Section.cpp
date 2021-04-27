fa2777ca9c8c3119075ad91871a533e62c17f659
yana-monet <80492399+yana-monet@users.noreply.github.com>
2021-04-27 08:52:18 -0400

    Update Section.cpp

--- Section.cpp
+++ Section.cpp
@@ -3,92 +3,28 @@
 
 #include "Section.h"
 #include <iostream>
+
+//cosntructor for Section takes in a bool based on whether the section is an intersection and sets occupied status to false
 Section::Section(bool intersection){
-  occupied =false;
+  occupied = false;
   this->intersection = intersection;
 }
-//Section::Section(Direction laneDirection){
-  //occupied = false;
-  //sectionDirection = laneDirection;
-//}
 
-/*Section::Section(int x){
-  //std::cout << "in Section constructor" << std::endl;
-  occupied = false;
-  int number = x;
- // std::cout << occupied << " in section" << std::endl; 
-}*/
-
-/*Section::Section(Section& copy)
- : occupied{copy.occupied}
-// , vehicleType{copy.vehicleType}
- , vehicle{copy.vehicle}
-// , vehicleDirection{copy.vehicleDirection}
-// , sectionDirection{copy.sectionDirection}
-// , turnStatus{copy.turnStatus}
-// , vehicleID{copy.vehicleID}
-   , intersection{copy.intersection} {}
-
-Section& Section::operator=(const Section& copy){
- if(this == &copy){
-  return *this;
-}
-
- occupied = copy.occupied;
-// vehicleDirection =  copy.vehicleDirection;
-// vehicleType = copy.vehicleType;
- vehicle = copy.vehicle;
-// sectionDirection = copy.sectionDirection;
-// turnStatus = copy.turnStatus;
-// vehicleID = copy.vehicleID;
-  intersection = copy.intersection;
-  return *this;
-}*/
-
-
-
+//default destructor for Section objects
 Section::~Section(){
-  //delete this; 
 }
 
+//returns whether Section is occupied or not
 bool Section::isOccupied(){
   return occupied;
 }
 
+//changes Section to occupied
 void Section::makeOccupied(){
   occupied = true;
-//  vehicleType = vehicleX.getVehicleType();
-//  vehicleID = vehicleX.getVehicleID();
-//  sectionDirection = direction;
-//  turnStatus = vehicleX.getTurningStatus();
-  //vehicle = vehicleX;
- // vehicleDirection = vehicleX.getVehicleOriginalDirection();
 }
 
-//bool Section::getTurningStatus(){
- // return turnStatus;
-//}
-
-//VehicleType Section::getVehicleType(){
- // return vehicle.getVehicleType();
-//}
-
-//Direction Section::getSectionDirection(){
- // return sectionDirection;
-//}
-
-//Direction Section::getVehicleDirection(){
- // return vehicleDirection;
-//}
-
-//int Section::getVehicleID(){
- // return vehicleID;
-//}
-
-//VehicleBase Section::getVehicle(){
- // return vehicle;
-//}
-
+//changes Section to unocupied
 void Section::unoccupy(){
   occupied = false;
 }
