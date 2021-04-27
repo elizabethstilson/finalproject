8063b3f283e1699a307acd1bd39af72f1845d4a7
elizabethstilson <80530571+elizabethstilson@users.noreply.github.com>
2021-04-27 11:09:54 -0400

    Update Lane.h

--- Lane.h
+++ Lane.h
@@ -16,22 +16,18 @@ class Lane{
     Direction laneDirection;
     int yellowNS;
     int yellowEW;
-    //Section emptySec;
-    //Section* temp;    
+    
+        
 
   public:
     std::vector<VehicleBase*> vehicles; 
     std::vector<Section> lane;
-   // std::vector<Section> lane;
     Lane(int halfsize, Direction direction, std::vector<VehicleBase*> vehicles);
     ~Lane();
     void assignVehicle(VehicleBase* vehicle);
     void moveLane(LightColor light, Lane* turnLane, Lane* straightLane, int yellow); // turn lane is first section in intersection straightLane is second
-    //void intersection(Section* curLaneSection, Lane* sharedLane, int interSection);
      void turnRight(VehicleBase* vehicle, Lane* turnLane);
       std::vector<Section> getLaneVector();
-   // void updateVector(std::vector<Section*> updatedVector);
-   // std::string getlaneDirection(); 
     std::string getLaneDirection(); 
     std::vector<VehicleBase*> getVehicleBase();
     bool partialVehicle(VehicleType firstVehicleType, VehicleBase* firstVehicle);
