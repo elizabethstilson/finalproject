6df6a10ca4c91977a734fe64f56c68295b5e75a8
yana-monet <80492399+yana-monet@users.noreply.github.com>
2021-04-27 08:53:07 -0400

    Update Section.h

--- Section.h
+++ Section.h
@@ -5,36 +5,19 @@
 #include <vector>
 #include "VehicleBase.h"
 
-//Declared in the VehicleBase class
-//enum class Direction   {north, south, east, west};
-
 class Section{
   private:
 
     bool occupied;
-    //VehicleType vehicleType;
-    //VehicleBase vehicle;
     bool intersection;
-   // Direction sectionDirection;
-   // Direction vehicleDirection;
-   // bool turnStatus;
-   // int vehicleID;
 
   public:
     Section(bool intersection);
-    //Section(Direction laneDirection);
-   // Section(Section& copy);
-   // Section& operator=(const Section& copy);
     ~Section();
 
     bool isOccupied();
     bool ifIntersection();
-   // bool getTurningStatus();
-    //VehicleType getVehicleType();
-   // Direction getSectionDirection();
-   // Direction getVehicleDirection();
-   // int getVehicleID();
-    //VehicleBase getVehicle();
+
     void unoccupy();
     void makeOccupied();
 };
