2add3d7804afb4c8885f70dc5dcf0d6abed9e0bd
elizabethstilson <80530571+elizabethstilson@users.noreply.github.com>
2021-04-27 12:44:19 -0400

    Update RandomNumber.h

--- RandomNumber.h
+++ RandomNumber.h
@@ -12,10 +12,6 @@ class RandomNumber{
     int initialSeed;
     mt19937 rng;
     string filename;
-    //const uniform_real_distribution<double> randomNum(0.0, 1.0);
-    //uniform_real_distribution<double> randomNum(double begin, double end);
-    //map<string, double> prob;
-    //Dictionary prob(string filename);
 
     RandomNumber(int seed, string filename); //set intial seed in constructor
     ~RandomNumber();
@@ -27,7 +23,6 @@ class RandomNumber{
     bool turnRightTruck();
     int getVehicle(); //0,1,2 (if else) -generate num beginning
     char getBound(); //n,s,e,w (if else)
-    //0-0.25 = nb, 0.26 - 0.35 = eb, 0.36 - 0.50 = wb, 0.51 - 0.65 = sb, 0.66+ none
     VehicleBase* whatVehicle();
 
 };
